3592f703b5ee03d25bc970c7bfa7ade0656d1dc2
Olha M <olha.melnichuk@gmail.com>
2016-12-08 22:41:24 +0200

    Update Task_add_6.java

--- src/homework_additional/Task_add_6.java
+++ src/homework_additional/Task_add_6.java
@@ -4,4 +4,15 @@ package homework_additional;
  6) У вас есть скорость и растояние, за какое время вы приедете в пукт назначания
  */
 public class Task_add_6 {
+   public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("введите значение скорости км/ч");
+        int v = scan.nextInt();
+        System.out.println("введите значение расстояния км ");
+        int s = scan.nextInt();
+        int t = s / v;
+        System.out.println(t + " час");
+    }
 }
+
+
